9e3e3a22b1371e87042bb4990a17e75aed26c968
Alexander Kiseliev <i43ack@gmail.com>
2025-05-01 13:54:12 +0300

    init

---
+++ bin/hivault.dart
@@ -0,0 +1,5 @@
+import 'package:hivault/hivault.dart' as hivault;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hivault.calculate()}!');
+}
---
+++ test/hivault_test.dart
@@ -0,0 +1,8 @@
+import 'package:hivault/hivault.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
